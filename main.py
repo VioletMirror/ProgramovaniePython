7779541a39542c57f911c1448117c4bf3271a756
Mirrorslav <xkunovsky@stuba.sk>
2025-04-03 13:05:05 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,28 +1,56 @@
-# Zadania na hodinu 27.3. 2025
+# Zadania na hodinu 3.4. 2025
 
 """
 Úloha 1.
-Vytvorte funkciu, ktorá bude generovať náhodné čísla od 1 do 100. Funckia bude počítať ich súčet a skončí dokým súčet týchto čísel
-nebude rovný 1000. Ak by bol aktuálny súčet väčší ako 1000, začína znova.
-
+Majme slovník: {“mesto”:”Bratislava”, “dedina”: Vlkolínec} . Pridajte do slovníku akúkoľvek
+hodnotu s kľúčom štát.
 """
 
 """
 Úloha 2.
-Máme dané 2 polia.
-Pole1 = [10,20,30,45,100]
-Pole2 = [49, 104, 1004,55, 26]
+Majme slovník: 
 
-Výsledkom programu bude pole, ktoré je súčtom Pole1 a Pole2.
-
-Naším výsledným poľom bude [59, 134, 1034, 100, 126] 
-
+slovnik = {“mesto”:”Bratislava”, “dedina”: Vlkolinec} 
+ 
+Napíšte funkciu, ktorá dostane ako parameter názov slovenského mesta. Následne vráti True ak slovník obsahuje zadané
+mesto a False ak nie. 
 """
 
 """
 Úloha 3.
-Užívateľ zadá svoj rok narodenia. Program vypočíta, koľko rokov bude mať v roku 2155.
-Ďalej vypíše počet mesiacov, týždňov, dní, hodín, minút a sekúnd medzi rokom narodenia a rokom 2155.
+Vytvorte funkciu, ktorá sčíta počet všetkých obyvateľ miest v slovníku:
 
-pre zjednodušenie môžeme uvažovať, že rok má 365,25 dní a 52,143 týždňov.
+Obyvatelia = {"Bratislava":500000, "Pezinok":25000, "Presov": 100000, "Humenne": 40000}
+
+(výsledok 665 000)
 """
+
+"""
+Úloha 4.
+Ktoré mesto obsahuje najväčší počet obyvateľov? Napíšte funkciu, ktorá vráti mesto s najväčším počtom obyvateľov.
+
+Môžte použiť slovník:
+Obyvatelia = {"Bratislava":500000, "Pezinok":25000, "Presov": 100000, "Humenne": 40000}
+"""
+
+"""
+Úloha 5.
+Pomocou nasledujúci polí vytvorte slovník:
+
+keys = [1,2,3,4]
+values = ["one", "two", "three", "four"]
+"""
+
+
+"""
+Úloha 6.
+Funckia dostane ako parameter string. Do slovníka uložte aké písmená string obsahuje a aký je ich počet:
+
+Napríklad:
+slovo = “adam”
+
+výsledný slovník
+pismena = {“a”: 2, “d”: 1, “m”: 1}
+"""
+
+
